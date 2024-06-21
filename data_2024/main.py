965aa8a5073a1b79480a333acdaebb45cb606235
justusm00 <j.multhaup2000@gmail.com>
2024-06-21 21:33:08 +0200

    soil heatflux now completed correclty (?)

--- data_2024/main.py
+++ data_2024/main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from preprocessing import prepare_data
-from soil import fill_thermal_conductivity
+from soil import fill_thermal_conductivity, compute_soil_heatflux, compute_soil_heatflux2
 
 # read data
 df_flux_fbg = pd.read_csv("data_2024/EddyCovarianceData/eng/FBG_fluxes_30min_20240401_20240608_eng.csv")
@@ -14,18 +15,35 @@ df_meteo_goew = pd.read_csv("data_2024/MeteorologicalData/eng/GoeW_meteo_30min_2
 # prepare data
 df_flux_fbg, df_flux_goew, df_meteo_fbg, df_meteo_goew = prepare_data(df_flux_fbg, df_flux_goew, df_meteo_fbg, df_meteo_goew)
 
+
 # compute thermal conductivity
 df_meteo_goew = fill_thermal_conductivity(df_meteo_goew)
 # df_meteo_goew.to_csv("meteo_with_conductivity.csv")
 
 # visualize thermal conductivity
-plt.figure()
-plt.plot(df_meteo_goew["TIMESTAMP_START"], df_meteo_goew["thermalConductivity_1_5cm"], label = "5cm")
-plt.plot(df_meteo_goew["TIMESTAMP_START"], df_meteo_goew["thermalConductivity_1_15cm"], label = "15cm")
+# plt.figure()
+# plt.plot(df_meteo_goew["TIMESTAMP_START"], df_meteo_goew["thermalConductivity_1_5cm"], label = "5cm")
+# plt.plot(df_meteo_goew["TIMESTAMP_START"], df_meteo_goew["thermalConductivity_1_15cm"], label = "15cm")
 
-plt.plot(df_meteo_goew["TIMESTAMP_START"], df_meteo_goew["thermalConductivity_1_30cm"], label = "30cm")
+# plt.plot(df_meteo_goew["TIMESTAMP_START"], df_meteo_goew["thermalConductivity_1_30cm"], label = "30cm")
 
 
-plt.xticks([])
-plt.legend()
-plt.show()
+# plt.xticks([])
+# plt.legend()
+# plt.show()
+
+
+# compute soil heatflux
+df_meteo_goew = compute_soil_heatflux(df_meteo_goew)
+
+def plot_diurnal_cycles_soil(df):
+    df_agg = df.groupby("TIME_START").agg(G=("soilHeatflux", "mean"), moisture=("soilMoisture_1_5cm", "mean"), k=("thermalConductivity_5cm_mean", "mean"), dTdz = ("dTdz_mean", "mean")).reset_index()
+    plt.plot(df_agg["TIME_START"], df_agg["G"])
+    plt.xticks(df_agg["TIME_START"].unique()[::2], rotation = 90)
+    plt.ylabel("$G$")
+    plt.tight_layout()
+    plt.show()
+    return df_agg
+
+df_agg = plot_diurnal_cycles_soil(df_meteo_goew)
+#df_agg.to_csv("bla.csv")
--- data_2024/soil.py
+++ data_2024/soil.py
@@ -2,7 +2,7 @@
 kw = 0.57 
 k_quartz = 8.8
 k_clay = 2.92
-n = 0.4 # porosity, literature value?
+n = 0.4 # porosity, value is more or less arbitrary here ->  TODO find actual value
 
 
 def get_soil_texture(depth):
@@ -92,9 +92,71 @@ def fill_thermal_conductivity(df):
     depths = [5, 15, 30] # cm
     for measure_idx in measure_indices:
         for depth in depths:
-            df[f"thermalConductivity_{measure_idx}_{depth}cm"] = df.apply(lambda x: get_thermal_conductivity(x[f"soilMoisture_{measure_idx}_{depth}cm"] / 100, depth), 
-                        axis=1)
+            df[f"thermalConductivity_{measure_idx}_{depth}cm"] = df.apply(lambda x: get_thermal_conductivity(x[f"soilMoisture_{measure_idx}_{depth}cm"] / 100, depth), axis=1)
             
     return df
 
 
+
+def compute_soil_heatflux(df):
+    """Compute soil heatflux 
+    Compute gradient at every measuring site and then average
+    Use only data at 5 and 15 cm depth
+
+    Args:
+        df (_type_): _description_
+    """
+    measure_indices = [1, 2, 3]
+    # compute gradient at every measurement site
+    for measure_idx in measure_indices:
+        df[f"dTdz_5cm_15cm_{measure_idx}"] = (df[f"soilTemperature_{measure_idx}_15cm"] - df[f"soilTemperature_{measure_idx}_5cm"]) / 0.1
+        # df[f"dTdz_5cm_30cm_{measure_idx}"] = (df[f"soilTemperature_{measure_idx}_5cm"] - df[f"soilTemperature_{measure_idx}_30cm"]) / 0.25
+        # df[f"dTdz_15cm_30cm_{measure_idx}"] = (df[f"soilTemperature_{measure_idx}_15cm"] - df[f"soilTemperature_{measure_idx}_30cm"]) / 0.15
+
+    # compute mean thermal conductivity at 5cm
+    df["thermalConductivity_5cm_mean"] = df[["thermalConductivity_1_5cm",
+                                             "thermalConductivity_2_5cm",
+                                             "thermalConductivity_3_5cm"]].mean(axis=1)
+
+
+    # compute mean gradient
+    # df["dTdz_mean"] = df[["dTdz_5cm_15cm_1", "dTdz_5cm_15cm_2", "dTdz_5cm_15cm_3",
+    #                       "dTdz_5cm_30cm_1", "dTdz_5cm_30cm_2", "dTdz_5cm_30cm_3",
+    #                       "dTdz_15cm_30cm_1", "dTdz_15cm_30cm_2", "dTdz_15cm_30cm_3"]].mean(axis = 1)
+    
+    df["dTdz_mean"] = df[["dTdz_5cm_15cm_1", "dTdz_5cm_15cm_2", "dTdz_5cm_15cm_3"]].mean(axis = 1)
+
+
+    df["soilHeatflux"] = -1 * df["thermalConductivity_5cm_mean"] * df["dTdz_mean"]
+    return df
+
+
+def compute_soil_heatflux2(df):
+    """Compute soil heatflux 
+    Average temperatures over all three sites and then compute gradient
+    Use only data at 5 and 15 cm depth
+
+    Args:
+        df (_type_): _description_
+    """
+    measure_indices = [1, 2, 3]
+    # compute gradient at every measurement site
+    df["soilTemperature_5cm_mean"] = df[["soilTemperature_1_5cm", "soilTemperature_2_5cm", "soilTemperature_3_5cm"]].mean(axis=1)
+    df["soilTemperature_15cm_mean"] = df[["soilTemperature_1_15cm", "soilTemperature_2_15cm", "soilTemperature_3_15cm"]].mean(axis=1)
+
+    # compute mean thermal conductivity at 5cm
+    df["thermalConductivity_5cm_mean"] = df[["thermalConductivity_1_5cm",
+                                             "thermalConductivity_2_5cm",
+                                             "thermalConductivity_3_5cm"]].mean(axis=1)
+
+
+    # compute mean gradient
+    # df["dTdz_mean"] = df[["dTdz_5cm_15cm_1", "dTdz_5cm_15cm_2", "dTdz_5cm_15cm_3",
+    #                       "dTdz_5cm_30cm_1", "dTdz_5cm_30cm_2", "dTdz_5cm_30cm_3",
+    #                       "dTdz_15cm_30cm_1", "dTdz_15cm_30cm_2", "dTdz_15cm_30cm_3"]].mean(axis = 1)
+    
+    df["dTdz_mean"] = (df["soilTemperature_15cm_mean"] - df["soilTemperature_5cm_mean"]) / 0.1
+
+
+    df["soilHeatflux"] = -1 * df["thermalConductivity_5cm_mean"] * df["dTdz_mean"]
+    return df
