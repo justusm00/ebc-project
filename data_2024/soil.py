a29978d66e6edd001d7bb5db0b580a531786dc1e
justusm00 <j.multhaup2000@gmail.com>
2024-06-24 17:35:47 +0200

    fixed thermal conductivity computation, still not consistent with literature values

--- data_2024/soil.py
+++ data_2024/soil.py
@@ -1,7 +1,7 @@
-# thermal conductivity of water, quartz and air ( at 20 °C) in Watts per meter per Kelvin
-kw = 0.57 
+# thermal conductivity of water, quartz and other materials in Watts per meter per Kelvin
+k_water = 0.57 
 k_quartz = 8.8
-k_clay = 2.92
+k_other = 2.92
 n = 0.4 # porosity, value is more or less arbitrary here ->  TODO find actual value
 
 
@@ -22,28 +22,29 @@ def get_soil_texture(depth):
     assert depth < 90
     
     if depth < 5:
-        x_clay = xs * 0.36
-        x_sand = xs * 0.64
+        x_other = xs * 0.36
+        x_quartz = xs * 0.64
     elif depth < 10:
-        x_clay = xs * 0.41
-        x_sand = xs * 0.59
+        x_other = xs * 0.41
+        x_quartz = xs * 0.59
     elif depth < 20:
-        x_clay = xs * 0.39
-        x_sand = xs * 0.61
+        x_other = xs * 0.39
+        x_quartz = xs * 0.61
     elif depth < 30:
-        x_clay = xs * 0.54
-        x_sand = xs * 0.46
+        x_other = xs * 0.54
+        x_quartz = xs * 0.46
     elif depth < 60:
-        x_clay = xs * 0.3
-        x_sand = xs * 0.7   
+        x_other = xs * 0.3
+        x_quartz = xs * 0.7   
     else: 
-        x_clay = xs * 0.23
-        x_sand = xs * 0.77
+        x_other = xs * 0.23
+        x_quartz = xs * 0.77
 
-    return x_clay, x_sand 
+    return x_quartz, x_other
 
 
-def get_thermal_conductivity(xw, depth):
+
+def get_thermal_conductivity(x_water, depth):
     """Compute thermal conductivity according to De Vries model:
     DeVries D.A. (1963) Thermal Properties of Soils. In W.R. van Wijk (ed.) Physics of Plant Environment. North-Holland Publishing Company, Amsterdam.
 
@@ -53,28 +54,34 @@ def get_thermal_conductivity(xw, depth):
     Args:
         water_content (_type_): _description_
     """
+
     # compute air content (= porosity minus water content)
-    xa = n - xw
-    # compute soil content (1 - porosity)
-    xs = 1 - n
+    x_air = n - x_water
+
     # compute dry soil texture 
-    x_clay, x_sand = get_soil_texture(depth)
-    # compute dry soil thermal conductivity
-    ks = x_clay * k_clay + x_sand * k_quartz
-    # define weighting factors
-    Fs = 1 / 3 * (2 / (1+ 0.125 * (ks / kw - 1)) + 1 / (1+ 0.75 * (ks / kw - 1)))
-    if xw > 0.09:
-        ga = 0.333 - xa / n * (0.333 - 0.035) # found conflicting info, best double check 
+    x_quartz, x_other = get_soil_texture(depth)
+
+    # compute air thermal conductivity at 4.4 degrees
+    if x_water > 0.09:
+        k_air = 0.059 + 0.066
     else:
-        ga = 0.013 + 0.944 * xw
+        k_air = 0.059 + 0.066 * x_water / 0.03
+        
+    # weighting factor for quartz
+    F_quartz = 1 / 3 * (2 / (1+ 0.125 * (k_quartz / k_water - 1)) + 1 / (1+ 0.75 * (k_quartz / k_water - 1)))
+    # weighting factor for other materials
+    F_other = 1 / 3 * (2 / (1+ 0.125 * (k_other / k_water - 1)) + 1 / (1+ 0.75 * (k_other / k_water - 1)))
+
+    # weighting factor for air
+    if x_water > 0.09:
+        ga = 0.333 - x_air / n * (0.333 - 0.035) # found conflicting info, best double check 
+    else:
+        ga = 0.013 + 0.944 * x_water
     gc = 1 - 2 * ga
-    # compute effective thermal conductivity of air due to humidity
-    # ka = 0.0615 + 1.9 * xw
-    ka = 0.025
-    Fa = 1 / 3 * (2 / (1+ ga * (ka / kw - 1)) + 1 / (1+ gc * (ka / kw - 1)))
+    F_air = 1 / 3 * (2 / (1+ ga * (k_air / k_water - 1)) + 1 / (1+ gc * (k_air / k_water - 1)))
     
     # compute thermal conductivity
-    k = (kw * xw + Fa * ka * xa + Fs * ks * xs) / (xw + Fa * xa + Fs * xs)
+    k = (k_water * x_water + F_air * k_air * x_air + F_quartz * k_quartz * x_quartz + F_other * k_other * x_other) / (x_water  + F_air * x_air + F_quartz * x_quartz + F_other * x_other)
     return k
 
 
