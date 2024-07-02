058629567859de94dfb801997592249bc70d5122
justusm00 <j.multhaup2000@gmail.com>
2024-07-02 15:58:46 +0200

    changed preprocess_data.py to preprocess_flux_data.py, outputs two files (one for training and one for prediction)

--- preprocess_flux_data.py
+++ preprocess_flux_data.py
@@ -6,22 +6,22 @@ from modules.util import transform_timestamp, numerical_to_float
 
 PATH = 'data/data_files/'
 # numerical values to transform to float
-COLS_NUMERICAL_PREP = ['H_orig', 'LE_orig', 'ET_orig', 'CO2', 'H2O', 'NEE_orig', 'Reco', 'GPP_f', 'Ustar']
-COLS_NUMERICAL_ORIG = ['H_orig', 'LE_orig', 'ET_orig', 'CO2', 'H2O', 'NEE_orig', 'Reco', 'GPP_f', 'Ustar', 'H_f', 'LE_f', 'ET_f', 'NEE_f']
+COLS_NUMERICAL = ['H_orig', 'LE_orig', 'ET_orig', 'CO2', 'H2O', 'NEE_orig', 'Reco', 'GPP_f', 'Ustar', 'H_f', 'LE_f', 'ET_f', 'NEE_f']
 
 # unnecessary columns to be dropped
-COLS_DROP_PREP = ['TIMESTAMP_START', 'TIMESTAMP_MITTE', 'TIMESTAMP_ENDE', 'H_f', 'LE_f', 'ET_f', 'NEE_f']
 COLS_DROP_ORIG = ['TIMESTAMP_START', 'TIMESTAMP_MITTE', 'TIMESTAMP_ENDE']
+# additional columns to be dropped for training data
+COLS_DROP_PREP = ['H_f', 'LE_f', 'ET_f', 'NEE_f', 'GPP_f']
 
 # collect files to preprocess
 files = [f for f in os.listdir(PATH) if 'fluxes' in f]
 
 
-data = []
+data_prep = []
+data_orig = []
 count_na = []
 
 for f in tqdm(files):
-    """ For united dataset """
     try: 
         df = pd.read_csv(PATH+f, sep=',').drop(0)
     except pd.errors.ParserError: 
@@ -33,7 +33,13 @@ for f in tqdm(files):
     df['location'] = '0' if 'BG' in f else '1'
 
     df = transform_timestamp(df, 'TIMESTAMP_START')
-    df = numerical_to_float(df, COLS_NUMERICAL_PREP)
+    df = numerical_to_float(df, COLS_NUMERICAL)
+
+
+    df.drop(COLS_DROP_ORIG, axis=1, inplace=True)
+    data_orig.append(df.copy())
+
+    # drop further columns
     df.drop(COLS_DROP_PREP, axis=1, inplace=True)
 
     # drop any row containing NA values
@@ -42,27 +48,7 @@ for f in tqdm(files):
     na_removed = len_before - df.__len__()
     count_na.append(na_removed)
 
-    data.append(df)
-
-    """ For preparing the original data for fitting and method comparison """
-    try: 
-        df = pd.read_csv(PATH+f, sep=',', na_values=['NaN']).drop(0)
-    except pd.errors.ParserError: 
-        # the 2024 files use ';' as separator and ',' as decimal separator
-        df = pd.read_csv(PATH+f, sep=';', na_values=['NaN']).drop(0)
-
-    df['location'] = '0' if 'BG' in f else '1'
-
-    df = transform_timestamp(df, 'TIMESTAMP_START')
-    df = numerical_to_float(df, COLS_NUMERICAL_ORIG)
-    df.drop(COLS_DROP_ORIG, axis=1, inplace=True)
-
-    # rename the files to name_mod.csv
-    name, csv = os.path.splitext(f)
-    mod_name = name + '_mod' + csv
-
-    df.to_csv(os.path.join("data/data_files_modified/", mod_name))
-
+    data_prep.append(df.copy())
 
 
     print(f'{f} done')
@@ -70,12 +56,11 @@ for f in tqdm(files):
 print(f'\nRows removed because of NA: {sum(count_na)}\n')
 
 # combine the preprocessed data into single dataframe
-data_final = pd.concat(data, axis=0, ignore_index=True)
+data_prep_final = pd.concat(data_prep, axis=0, ignore_index=True)
+data_orig_final = pd.concat(data_orig, axis=0, ignore_index=True)
 
 
 
 
-
-
-
-data_final.to_csv('data/data_preprocessed.csv')
+data_prep_final.to_csv('data/flux_data_preprocessed.csv')
+data_orig_final.to_csv('data/flux_data_preprocessed_gapfilled.csv')
