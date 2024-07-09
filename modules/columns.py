58286cd481f79e4871ffc4d1c063cfd621aae490
justusm00 <j.multhaup2000@gmail.com>
2024-07-09 09:59:19 +0200

    fixed spelling error

--- modules/columns.py
+++ modules/columns.py
@@ -5,10 +5,10 @@ COLS_KEY_ALT = ["year", "day_of_year", "30min", "location"]
 # all possible columns that can be used as labels for mlp training 
 COLS_LABELS_ALL = ["H_orig", "LE_orig"]
 
-# all possible columns tha cant be used as features for mlp training
-COLS_FEATURES_ALL = COLS_KEY + ["netRadiation", "incomingShortwaveRadiation", "soilHeatflux", "airPressure",
-                 "waterPressureDeficit", "waterVaporPressure", "windSpeed", "relativeHumidity", "day_of_year"]
-# COLS_FEATURES = COLS_KEY + ["incomingShortwaveRadiation", "soilHeatflux", "waterPressureDeficit", "windSpeed"] # Results from importance analysis
+# all possible columns that can be used as features for mlp training
+COLS_FEATURES_ALL = COLS_KEY + ["netRadiation", "incomingShortwaveRadiation",
+                                "soilHeatflux", "airPressure", 
+                                "waterPressureDeficit", "waterVaporPressure", "windSpeed", "relativeHumidity", "day_of_year"]
 
 # columns to keep from flux data
 # allowed columns: "H_orig", "LE_orig", "H_f", "LE_f", 'CO2', 'H2O', 'Ustar', 'Reco'
